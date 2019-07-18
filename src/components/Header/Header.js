3d6b78c81c60ac030a45b523f5e796df016ea71d
Brandon Desselle <desselle.brandon2@gmail.com>
2019-07-18 17:03:07 -0500

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export default function Header(){
-    return (
-        <div>
-          <img src="../../../assets/nasa_logo.png" alt="Nasa Logo"></img>
-        </div>
-    )
+export default function Header() {
+  return (
+    <div>
+      <img src="https://api.nasa.gov/images/logo.png" alt="Nasa Logo"></img>
+    </div>
+  )
 }
