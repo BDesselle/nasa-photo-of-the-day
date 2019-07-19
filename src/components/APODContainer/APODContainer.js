bd601ac8abd22bf5df605819f9686d38204ad716
Brandon Desselle <desselle.brandon2@gmail.com>
2019-07-19 10:30:55 -0500

    Update APODContainer.js

--- src/components/APODContainer/APODContainer.js
+++ src/components/APODContainer/APODContainer.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { Image, Card, Modal, Label } from 'semantic-ui-react';
 
 
 export default function APODContainer() {
@@ -20,10 +21,35 @@ export default function APODContainer() {
   }, [date]); // Using the {date} state as a dependency to syncronize my useEffect
 
   return (
-    <div>
-      <h2>{data.title}</h2>
-      <img alt='NASA APOD' src={data.url} style={{ maxWidth: "370px" }} />
-      <p>{data.explanation}</p>
-    </div>
-  );
+    < div >
+      <Modal size='fullscreen' dimmer='blurring' trigger={
+        <Card centered>
+          <Label style={{ maxWidth: "50%" }} as='a' size='tiny' color='red' ribbon>
+            Click to read more!
+          </Label>
+          <Card.Content>
+            <Card.Header>{data.title}</Card.Header>
+            <Card.Description>
+              {date}
+            </Card.Description>
+          </Card.Content>
+          <Image alt='NASA APOD' src={data.url} />
+          <Card.Content extra>
+            <Card.Meta>
+              <span className='date'>© 2019 NASA</span>
+            </Card.Meta>
+          </Card.Content>
+        </Card>
+      }>
+        <Modal.Header>{data.title}</Modal.Header>
+        <Image alt='NASA APOD' src={data.url} />
+        <Modal.Content>
+          <Modal.Description>
+            <p>{data.explanation}</p>
+          </Modal.Description>
+        </Modal.Content>
+      </Modal>
+      {/* <p>{data.explanation}</p> */}
+    </div >
+  )
 }
